0266c34a54b9398de1d4a2489553519a7a84489a
Ryan Clarian <rclarian@gmail.com>
2024-07-26 22:42:27 +0800

    #9. API - Adding our first API Controller
    #9. API - Adding our first API Controller

---
+++ API/Skinet.API/Skinet.API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skinet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "This will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "This will be a product";
+        }
+
+    }
+}
