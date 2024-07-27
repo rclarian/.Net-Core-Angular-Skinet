7eddb6a53c0f55d29b27da9fb4260e9a6b6672a6
Ryan Clarian <rclarian@gmail.com>
2024-07-27 11:19:00 +0800

    #11. API - Setting up Entity Framework
    #11. API - Setting up Entity Framework

---
+++ API/Skinet.API/Skinet.API/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skinet.API.Entities;
+
+namespace Skinet.API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
