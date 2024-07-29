3a2b671a77623672aae6c11b1f46cf9d8f2cf557
Ryan Clarian <rclarian@gmail.com>
2024-07-29 11:12:52 +0800

    #26. API - Configuring the migrations
    #26. API - Configuring the migrations

--- API/Skinet.API/Infrastructure/Data/Migrations/20240729030624_Adding Entities.Designer.cs
+++ API/Skinet.API/Infrastructure/Data/Migrations/20240729030624_Adding Entities.Designer.cs
@@ -3,6 +3,7 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240729030624_Adding Entities")]
+    partial class AddingEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
